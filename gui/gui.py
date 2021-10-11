043f0c7cce30faaae50bb4fe977b389ed94bf59d
thewomins <thewomins@gmail.com>
2021-10-11 13:41:47 -0300

    add menus to gui controller

--- gui/gui.py
+++ gui/gui.py
@@ -3,9 +3,15 @@ import sys
 import time
 import threading
 
-import navegador.navegador as navegador
 from tkinter import messagebox
+from gui.menu_agregar_dolar import Menu_agregar_dolar
+from gui.menu_lateral import Menu_lateral
+from gui.menu_principal import Menu_principal
+from gui.menu_agregar_crypto import Menu_agregar_crypto
 from controllers.main_view_controller import Controller
+from controllers.add_dolar_controller import New_dolar
+from controllers.menu_lat_controller import Lat_controller
+from controllers.add_crypto_controller import New_crypto
 
 class Gui:
     def __init__(self, wallet):
@@ -15,16 +21,33 @@ class Gui:
         ventana = tk.Tk()
         ventana.minsize(750, 500)
         self.vent=ventana
+    
+    def actualizar_wallet(self):
+        self.wallet.actualizar_datos()
+
+    def mostrar_menu_lat(self):
+        controller = Lat_controller()
+        menu_lat = Menu_lateral(self.vent, controller)
+        menu_lat.frame_menu_lateral().place(relheight=1, relwidth=0.32)
 
     def ventana_loop(self):
         self.vent.mainloop()
 
     def home_page(self):
-        menu_principal = tk.Frame(self.vent)
-        menu_principal.place(relheight=1,relwidth=1)
-        controller_main = Controller(menu_principal, self.wallet)
-        controller_main.home_view()
-        
-    def prueba(self):
-        pagina= tk.Frame(self.vent)
-        pagina.place(relheight=1,relwidth=1)
+        control_menu=Controller(self.wallet)
+        self.mostrar_menu_lat()
+        menu_principal = Menu_principal(self.vent, control_menu)
+        view=menu_principal.menu()
+        view.place(relx=0.32, relheight=1, relwidth=0.68)
+
+    def menu_compra_dolar(self):
+        control_menu=New_dolar(self.wallet)
+        menu=Menu_agregar_dolar(self.vent, control_menu)
+        self.mostrar_menu_lat()
+        menu.interior().place(relx=0.32, relheight=1, relwidth=0.68)
+
+    def menu_compra_crypto(self):
+        control_menu=New_crypto(self.wallet)
+        menu = Menu_agregar_crypto(self.vent, control_menu)
+        self.mostrar_menu_lat()
+        menu.interior().place(relx=0.32, relheight=1, relwidth=0.68)
